49c8ab3c329caec9422afdac98a92994dcfabe72
nmmz76 <nmmz76@mst.edu>
2020-05-10 02:44:36 -0500

    wifi

--- utilities.py
+++ utilities.py
@@ -1,5 +1,45 @@
 import numpy as np
 import time
+import RPi.GPIO as GPIO
+import threading
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+class Blinker(threading.Thread):
+    def __init__(self, pin, hz=1):
+        threading.Thread.__init__(self)
+        self.state = False
+        self.time = 0.5/hz
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, False)
+        self.daemon = True  # Allow main to exit even if still running.
+        self.paused = True  # Start out paused.
+        self.cond = threading.Condition()
+
+    def run(self):
+        while True:
+            with self.cond:
+                if self.paused:
+                    self.cond.wait()
+            self.state = not self.state
+            GPIO.output(self.pin,self.state)
+            time.sleep(self.time)
+
+    def pause(self):
+        self.state = False
+        GPIO.output(self.pin, self.state)
+        with self.cond:
+            self.paused = True
+
+    def resume(self):
+        with self.cond:
+            self.paused = False
+            self.cond.notify()
+
+    def change_freq(self,hz):
+        self.time = 0.5/hz
 
 class Clock:
     def __init__(self,t=None):
---
+++ wifi_camcorder.py
@@ -0,0 +1,62 @@
+import os
+from PyAccessPoint import pyaccesspoint
+import time
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+import threading
+import time
+import os
+import datetime
+from subprocess import call
+from utilities import Blinker
+
+GPIO.setmode(GPIO.BCM)
+
+class distributed_camcorder:
+    def __init__(self):
+        self.access_point = None
+        self.type = 'client'
+        self.red_led = Blinker(13,hz=0.5)
+        self.green_led = Blinker(19,hz=0.5)
+        self.green_led.start()
+        self.red_led.start()
+        self.red_led.resume()
+        time.sleep(0.25)
+        self.green_led.resume()
+        time.sleep(5)
+        self.green_led.stop()
+        self.red_led.stop()
+
+        if self.configure_network():
+            led = self.green_led
+        else:
+            led = self.red_led
+        led.change_freq(0.2)
+        led.resume()
+        time.sleep(3)
+        led.stop()
+
+
+    def create_access_point(self):
+        self.access_point = pyaccesspoint.AccessPoint(ssid='PiCam-net',password='spectral')
+        self.access_point.start()
+        time.sleep(3)
+        return self.access_point.is_running()
+
+    def configure_network(self):
+        ontest = os.popen("ping -c 1 192.168.45.1").read()
+        if ontest == '':
+            print("No network available. Creating new network")
+            if self.create_access_point():
+                self.type = 'server'
+                print('Network successfully created')
+            else:
+                print('Failed to create network. Defaulting to client')
+                return False
+        else:
+            print("Connected successfully!")
+        return True
+
+
+if __name__ == '__main__':
+    distributed_camcorder()
