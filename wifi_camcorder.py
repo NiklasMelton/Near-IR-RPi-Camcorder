1a4904e50d227e3d996622fd5352b6c4bfb34199
nmmz76 <nmmz76@mst.edu>
2020-05-10 03:20:42 -0500

    wifi

--- wifi_camcorder.py
+++ wifi_camcorder.py
@@ -37,8 +37,8 @@ class distributed_camcorder:
 
 
     def create_access_point(self):
-        os.popen("ifconfig wlan0 down")
-        time.sleep(5)
+        # os.popen("ifconfig wlan0 down")
+        # time.sleep(5)
         self.access_point = pyaccesspoint.AccessPoint(ssid='PiCam-net',password='spectral')
         self.access_point.start()
         time.sleep(3)
